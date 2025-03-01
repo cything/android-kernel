3c4d49bbed7eee8c2d294f116a76bdafb2cf8330
cy <cy@cy7.sh>
2025-03-01 11:31:17 -0500

    init docker support
    use my fork for the builder
    move oem-boot.img to assets/

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nix-kernelsu-builder.url = "github:xddxdd/nix-kernelsu-builder";
+    nix-kernelsu-builder.url = "github:cything/ksu-builder";
   };
   outputs =
     { flake-parts, ... }@inputs:
@@ -30,7 +30,21 @@
               ];
               kernelImageName = "Image";
               kernelSrc = ./.;
-              oemBootImg = ./oem-boot.img;
+              oemBootImg = ./assets/oem-boot.img;
+              kernelConfig = ''
+                CONFIG_PID_NS=y
+                CONFIG_IPC_NS=y
+                CONFIG_CGROUP_DEVICE=y
+                CONFIG_BRIDGE_NETFILTER=y
+                CONFIG_NETFILTER_XT_MATCH_ADDRTYPE=y
+                CONFIG_NETFILTER_XT_MATCH_IPVS=y
+                CONFIG_POSIX_MQUEUE=y
+                CONFIG_VXLAN=y
+                CONFIG_BRIDGE_VLAN_FILTERING=y
+                CONFIG_IPVLAN=y
+                CONFIG_MACVLAN=y
+                CONFIG_BTRFS_FS_POSIX_ACL=y
+              '';
             };
           };
         };
