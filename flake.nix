92a1ce5587016e6972a34d14f1152a6eba257764
cy <cy@cy7.sh>
2025-03-01 19:04:56 -0500

    move docker config stuff to defconfig

--- flake.nix
+++ flake.nix
@@ -44,20 +44,6 @@
               kernelImageName = "Image";
               kernelSrc = ./.;
               oemBootImg = ./assets/oem-boot.img;
-              kernelConfig = ''
-                CONFIG_PID_NS=y
-                CONFIG_IPC_NS=y
-                CONFIG_CGROUP_DEVICE=y
-                CONFIG_BRIDGE_NETFILTER=y
-                CONFIG_NETFILTER_XT_MATCH_ADDRTYPE=y
-                CONFIG_NETFILTER_XT_MATCH_IPVS=y
-                CONFIG_POSIX_MQUEUE=y
-                CONFIG_VXLAN=y
-                CONFIG_BRIDGE_VLAN_FILTERING=y
-                CONFIG_IPVLAN=y
-                CONFIG_MACVLAN=y
-                CONFIG_BTRFS_FS_POSIX_ACL=y
-              '';
             };
           };
         };
